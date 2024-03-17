a7ae895977aac60ace2df367965b2dd2b90c0c3a
gb-0001 <82384062+gb-0001@users.noreply.github.com>
2024-03-17 02:20:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-sudo cp -rf deepcool-ak620-digital.service /lib/systemd/system/
-sudo cp -rf deepcool-ak620-digital-restart.service /lib/systemd/system/
-sudo cp -rf deepcool-ak620-digital.py /usr/bin/deepcool-ak620-digital.py
+cp -rf deepcool-ak620-digital.service /lib/systemd/system/
+cp -rf deepcool-ak620-digital-restart.service /lib/systemd/system/
+cp -rf deepcool-ak620-digital.py /usr/bin/deepcool-ak620-digital.py
 
 
-
-# Créer un environnement virtuel Python
+apt install -y python3-venv
+# Creer un environnement virtuel Python
 /usr/bin/python3 -m venv ~/venv
+apt install -y python3-pip
 
 # Activer l'environnement virtuel
 source ~/venv/bin/activate
 
-# Installer les paquets Python nécessaires
+# Installer les paquets Python nÃ©cessaires
 pip install hid psutil
 
 # Modifier les fichiers de service
 sed -i 's|ExecStart=.*|ExecStart= /root/venv/bin/python /usr/bin/deepcool-ak620-digital.py|' /lib/systemd/system/deepcool-ak620-digital.service
 sed -i 's|ExecStart=.*|ExecStart= /root/venv/bin/python /usr/bin/deepcool-ak620-digital.py|' /lib/systemd/system/deepcool-ak620-digital-restart.service
 
-sudo chmod +x /usr/bin/deepcool-ak620-digital.py
+chmod +x /usr/bin/deepcool-ak620-digital.py
 
-sudo systemctl enable deepcool-ak620-digital.service
-sudo systemctl enable deepcool-ak620-digital-restart.service
+systemctl enable deepcool-ak620-digital.service
+systemctl enable deepcool-ak620-digital-restart.service
 
-# Redémarrer le service
-sudo systemctl daemon-reload
-sudo systemctl restart deepcool-ak620-digital.service
+# RedÃ©marrer le service
+systemctl daemon-reload
+systemctl restart deepcool-ak620-digital.service
+
+systemctl status deepcool-ak620-digital.service
